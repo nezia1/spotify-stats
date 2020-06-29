07a57ab73479c62063841fb740ea2dafb053b889
Anthony Rodriguez <anthony@nezia.xyz>
2020-06-29 18:03:38 +0200

    Add user profile fetch and name / avatar display

--- src/App.vue
+++ src/App.vue
@@ -10,9 +10,18 @@
       >
         <v-icon left>mdi-spotify</v-icon>Login with Spotify
       </v-btn>
-      <v-btn v-else color="primary" v-on:click="logout">
-        <v-icon left>mdi-logout</v-icon>Logout
-      </v-btn>
+      <v-row v-else justify="end" align="center">
+        <v-skeleton-loader :loading="isProfileLoading" type="avatar" class="mr-3">
+          <v-avatar size="40">
+            <img :src="userProfile.images[0].url" alt srcset />
+          </v-avatar>
+        </v-skeleton-loader>
+
+        <h3 class="mr-5 font-weight-regular">Welcome, {{userProfile.display_name}}</h3>
+        <v-btn color="primary" v-on:click="logout">
+          <v-icon left>mdi-logout</v-icon>Logout
+        </v-btn>
+      </v-row>
     </v-app-bar>
 
     <v-main>
@@ -22,9 +31,11 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: 'App',
-  created() {
+  async created() {
     this.$vuetify.theme.dark = window.matchMedia('(prefers-color-scheme: dark)').matches;
     if (this.$route.hash) {
       const regexToken = /(?:#access_token=)(?<accessToken>.+?)(?:&.*)/;
@@ -32,16 +43,28 @@ export default {
       this.spotifyToken = tokenMatch.groups.accessToken;
       if (this.spotifyToken) {
         this.$router.replace('');
+        this.userProfile = await this.fetchUserProfile();
       }
     }
   },
   data: () => ({
     spotifyToken: '',
     clientId: process.env.VUE_APP_CLIENT_ID,
+    userProfile: {},
+    isProfileLoading: true,
   }),
   methods: {
     logout() {
       this.spotifyToken = '';
+    },
+    async fetchUserProfile() {
+      const profile = await axios.get('https://api.spotify.com/v1/me', {
+        headers: {
+          Authorization: `Bearer ${this.spotifyToken}`,
+        },
+      });
+      this.isProfileLoading = false;
+      return profile.data;
     },
   },
 };
