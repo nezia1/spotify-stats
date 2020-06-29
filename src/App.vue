e8395ad4378a13f897cd2d5f25aea70ad7b80083
Anthony Rodriguez <anthony@nezia.xyz>
2020-06-29 13:38:16 +0200

    Add dark theme detector and remove template components

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,6 @@
   <v-app>
     <v-app-bar app>
       <v-toolbar-title>Spotify Charts</v-toolbar-title>
-      <v-spacer></v-spacer>
-
-      <v-btn href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank" text>
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
     </v-app-bar>
 
     <v-main>
@@ -20,7 +14,7 @@
 export default {
   name: 'App',
   created() {
-    this.$vuetify.theme.dark = false;
+    this.$vuetify.theme.dark = window.matchMedia('(prefers-color-scheme: dark)').matches;
   },
   data: () => ({
 
