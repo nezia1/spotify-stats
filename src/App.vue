288dd218314e68eb167662d0dc49487a7387386e
Anthony Rodriguez <anthony@nezia.xyz>
2020-06-29 17:06:38 +0200

    Move access token code to main App file, update authentication flow to use token and add regex to parse hash

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,17 @@
   <v-app>
     <v-app-bar app>
       <v-toolbar-title>Spotify Charts</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        v-if="!spotifyToken"
+        color="primary"
+        :href="`https://accounts.spotify.com/authorize?response_type=token&client_id=${clientId}&scope=user-read-private user-read-email&redirect_uri=http://localhost:8080`"
+      >
+        <v-icon left>mdi-spotify</v-icon>Login with Spotify
+      </v-btn>
+      <v-btn v-else color="primary" v-on:click="logout">
+        <v-icon left>mdi-logout</v-icon>Logout
+      </v-btn>
     </v-app-bar>
 
     <v-main>
@@ -15,9 +26,23 @@ export default {
   name: 'App',
   created() {
     this.$vuetify.theme.dark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    if (this.$route.hash) {
+      const regexToken = /(?:#access_token=)(?<accessToken>.+?)(?:&.*)/;
+      const tokenMatch = regexToken.exec(this.$route.hash);
+      this.spotifyToken = tokenMatch.groups.accessToken;
+      if (this.spotifyToken) {
+        this.$router.replace('');
+      }
+    }
   },
   data: () => ({
-
+    spotifyToken: '',
+    clientId: process.env.VUE_APP_CLIENT_ID,
   }),
+  methods: {
+    logout() {
+      this.spotifyToken = '';
+    },
+  },
 };
 </script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,33 +1,14 @@
 <template>
   <v-container>
-    <v-row class="text-center">
-      <v-btn
-        v-if="!spotifyCode"
-        color="primary"
-        href="https://accounts.spotify.com/authorize?response_type=code&client_id=af51cce79ce14914a5eba62f51c92a2b&scope=user-read-private user-read-email&redirect_uri=http://localhost:8080"
-      >Login</v-btn>
-      <v-btn v-else color="primary" v-on:click="logout">Logout</v-btn>
-    </v-row>
+    <v-row class="text-center"></v-row>
   </v-container>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  created() {
-    this.spotifyCode = this.$route.query.code;
-    if (this.spotifyCode) {
-      this.$router.replace('');
-    }
-  },
   data: () => ({
-    spotifyCode: '',
-    profile: {},
+
   }),
-  methods: {
-    logout() {
-      this.spotifyCode = '';
-    },
-  },
 };
 </script>
