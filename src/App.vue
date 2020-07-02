ba828a267e8fa21473b013ff0c849f936bd3a17d
Anthony Rodriguez <anthony@nezia.xyz>
2020-07-02 15:14:34 +0200

    Move data fetching to vuex

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
       <v-toolbar-title>Spotify Charts</v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn
-        v-if="!spotifyToken"
+        v-if="!accessToken"
         color="primary"
         :href="`https://accounts.spotify.com/authorize?response_type=token&client_id=${clientId}&scope=user-read-private user-top-read user-read-email&redirect_uri=http://localhost:8080`"
       >
@@ -13,12 +13,12 @@
       <v-row v-else justify="end" align="center">
         <v-skeleton-loader :loading="isProfileLoading" type="avatar" class="mr-3">
           <v-avatar size="40">
-            <img :src="userProfile.images[0].url" alt srcset />
+            <img :src="userProfile.images[0].url" :alt="`User profile picture`" srcset />
           </v-avatar>
         </v-skeleton-loader>
 
         <h3 class="mr-5 font-weight-regular">Welcome, {{userProfile.display_name}}</h3>
-        <v-btn color="primary" v-on:click="logout">
+        <v-btn color="primary" v-on:click="$store.commit('logout')">
           <v-icon left>mdi-logout</v-icon>Logout
         </v-btn>
       </v-row>
@@ -31,51 +31,32 @@
 </template>
 
 <script>
-import axios from 'axios';
-
 export default {
   name: 'App',
-  async created() {
+  async mounted() {
     this.$vuetify.theme.dark = window.matchMedia('(prefers-color-scheme: dark)').matches;
     if (this.$route.hash) {
       const regexToken = /(?:#access_token=)(?<accessToken>.+?)(?:&.*)/;
       const tokenMatch = regexToken.exec(this.$route.hash);
-      this.spotifyToken = tokenMatch.groups.accessToken;
-      if (this.spotifyToken) {
+      this.$store.commit('setAccessToken', tokenMatch.groups.accessToken);
+      if (this.accessToken) {
         this.$router.replace('');
-        this.userProfile = await this.fetchUserProfile();
+        await this.$store.dispatch('fetchUserProfile');
+        this.isProfileLoading = false;
       }
     }
   },
   data: () => ({
-    spotifyToken: '',
     clientId: process.env.VUE_APP_CLIENT_ID,
-    userProfile: {},
     isProfileLoading: true,
-    topTracks: {},
-    topArtists: {},
   }),
-  methods: {
-    logout() {
-      this.spotifyToken = '';
+  computed: {
+    accessToken() {
+      return this.$store.state.accessToken;
     },
-    async fetchUserProfile() {
-      const response = await axios.get('https://api.spotify.com/v1/me', {
-        headers: {
-          Authorization: `Bearer ${this.spotifyToken}`,
-        },
-      });
-      this.isProfileLoading = false;
-      return response.data;
-    },
-    async fetchTopTracks(limit = 50) {
-      const response = await axios.get(`https://api.spotify.com/v1/me/top/tracks?limit=${limit}`, {
-        headers: {
-          Authorization: `Bearer ${this.spotifyToken}`,
-        },
-      });
-      return response.data;
-    },
+    userProfile() {
+      return this.$store.state.userProfile;
+    }
   },
 };
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,50 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
+import Vue from "vue";
+import Vuex from "vuex";
+import axios from "axios";
+
+const SPOTIFY_URI = "https://api.spotify.com/v1/me";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    accessToken: "",
+    userProfile: {},
+    topTracks: {}
   },
   mutations: {
+    setAccessToken(state, token) {
+      state.accessToken = token;
+    },
+    setUserProfile(state, userProfile) {
+      state.userProfile = userProfile;
+    },
+    setTopTracks(state, topTracks) {
+      state.topTracks = topTracks;
+    },
+    logout(state) {
+      state.accessToken = "";
+    }
   },
   actions: {
+    async fetchUserProfile({ commit, state }) {
+      const response = await axios.get(`${SPOTIFY_URI}`, {
+        headers: {
+          Authorization: `Bearer ${state.accessToken}`
+        }
+      });
+      commit("setUserProfile", response.data);
+      return response;
+    },
+    async fetchTopTracks({ commit, state }, limit = 50) {
+      const response = await axios.get(`${SPOTIFY_URI}/top/tracks?limit=${limit}`, {
+        headers: {
+          Authorization: `Bearer ${state.accessToken}`
+        }
+      });
+      commit("setTopTracks", response.data);
+      return response.data;
+    }
   },
-  modules: {
-  },
+  modules: {}
 });
