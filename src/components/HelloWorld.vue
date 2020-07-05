f40fccfa280f7e5fe1401fee9f3cd23df17cfa1a
Anthony Rodriguez <anthony@nezia.xyz>
2020-07-05 14:54:02 +0200

    Update grid layout to work on mobile

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,64 +1,54 @@
 <template>
   <v-container>
     <v-row>
-      <v-row v-if="accessToken">
-        <v-col class="text-center">
-          <h1>Current</h1>
+      <v-col class="text-center">
+        <h1>Current</h1>
+        <v-col v-for="topTrack in topTracksCurrent" :key="topTrack.id">
+          <v-lazy>
+            <v-card color="cyan darken-2" class="white--text" height="100%">
+              <v-row fill-height align-center>
+                <v-col cols="5" height="100%">
+                  <v-img :src="topTrack.album.images[0].url" height="125px" contain></v-img>
+                </v-col>
+                <v-col cols="7">
+                  <v-fade-transition>
+                    <v-card-title primary-title class="text-left d-block">
+                      <div>
+                        <div class="headline text-truncate">{{topTrack.name}}</div>
+                        <div class="text-truncate">{{formattedArtistNames(topTrack.artists)}}</div>
+                        <div>{{topTrack.album.release_date.split('-')[0]}}</div>
+                      </div>
+                    </v-card-title>
+                  </v-fade-transition>
+                </v-col>
+              </v-row>
+            </v-card>
+          </v-lazy>
         </v-col>
-        <v-col class="text-center">
-          <h1>All time</h1>
+      </v-col>
+      <v-col class="text-center">
+        <h1>All time</h1>
+        <v-col v-for="topTrack in topTracksAllTime" :key="topTrack.id">
+          <v-lazy>
+            <v-card color="cyan darken-2" class="white--text" height="100%">
+              <v-row fill-height align-center>
+                <v-col cols="5" height="100%">
+                  <v-img :src="topTrack.album.images[0].url" height="125px" contain></v-img>
+                </v-col>
+                <v-col cols="7">
+                  <v-card-title primary-title class="text-left d-block">
+                    <div>
+                      <div class="headline text-truncate">{{topTrack.name}}</div>
+                      <div class="text-truncate">{{formattedArtistNames(topTrack.artists)}}</div>
+                      <div>{{topTrack.album.release_date.split('-')[0]}}</div>
+                    </div>
+                  </v-card-title>
+                </v-col>
+              </v-row>
+            </v-card>
+          </v-lazy>
         </v-col>
-      </v-row>
-      <v-fade-transition>
-        <v-row v-show="!isLoading">
-          <v-col>
-            <v-col v-for="topTrack in topTracksCurrent" :key="topTrack.id">
-              <v-lazy>
-                <v-card color="cyan darken-2" class="white--text" height="100%">
-                  <v-row fill-height align-center>
-                    <v-col cols="5" height="100%">
-                      <v-img :src="topTrack.album.images[0].url" height="125px" contain></v-img>
-                    </v-col>
-                    <v-col cols="7">
-                      <v-fade-transition>
-                        <v-card-title primary-title class="text-left d-block">
-                          <div>
-                            <div class="headline text-truncate">{{topTrack.name}}</div>
-                            <div class="text-truncate">{{formattedArtistNames(topTrack.artists)}}</div>
-                            <div>{{topTrack.album.release_date.split('-')[0]}}</div>
-                          </div>
-                        </v-card-title>
-                      </v-fade-transition>
-                    </v-col>
-                  </v-row>
-                </v-card>
-              </v-lazy>
-            </v-col>
-          </v-col>
-          <v-col>
-            <v-col v-for="topTrack in topTracksAllTime" :key="topTrack.id">
-              <v-lazy>
-                <v-card color="cyan darken-2" class="white--text" height="100%">
-                  <v-row fill-height align-center>
-                    <v-col cols="5" height="100%">
-                      <v-img :src="topTrack.album.images[0].url" height="125px" contain></v-img>
-                    </v-col>
-                    <v-col cols="7">
-                      <v-card-title primary-title class="text-left d-block">
-                        <div>
-                          <div class="headline text-truncate">{{topTrack.name}}</div>
-                          <div class="text-truncate">{{formattedArtistNames(topTrack.artists)}}</div>
-                          <div>{{topTrack.album.release_date.split('-')[0]}}</div>
-                        </div>
-                      </v-card-title>
-                    </v-col>
-                  </v-row>
-                </v-card>
-              </v-lazy>
-            </v-col>
-          </v-col>
-        </v-row>
-      </v-fade-transition>
+      </v-col>
     </v-row>
   </v-container>
 </template>
