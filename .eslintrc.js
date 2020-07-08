ffce0d22a13858396fe9728615302ba53810e01f
Anthony Rodriguez <anthony@nezia.xyz>
2020-07-09 01:55:17 +0200

    Upgrade ESLint and enable  support for ES2020

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,15 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
-  extends: ["plugin:vue/essential", "@vue/airbnb", "prettier"],
+  extends: ['plugin:vue/essential', '@vue/airbnb', 'prettier'],
   parserOptions: {
-    parser: "babel-eslint"
+    parser: 'babel-eslint',
+    ecmaVersion: 2020,
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
-  }
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+  },
 };
