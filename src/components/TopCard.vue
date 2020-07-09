ce774621ebe9824d690ded9fe5d3c40563191f64
Anthony Rodriguez <anthony@nezia.xyz>
2020-07-09 02:40:40 +0200

    Remove fade transition

--- src/components/TopCard.vue
+++ src/components/TopCard.vue
@@ -1,24 +1,22 @@
 <template>
-  <v-fade-transition>
-    <v-card :color="cardColor" class="white--text" height="100%">
-      <v-row>
-        <v-col cols="5">
-          <v-img :src="isTrack ? element.album.images[0].url : ''" height="150px" contain></v-img>
-        </v-col>
-        <v-col cols="7">
-          <v-card-title primary-title class="text-left d-block">
-            <div>
-              <div class="headline text-truncate">{{ element.name }}</div>
-              <div class="text-truncate">
-                {{ isTrack ? formattedArtistNames(element.artists) : '' }}
-              </div>
-              <div>{{ isTrack ? element.album.release_date.split('-')[0] : '' }}</div>
+  <v-card :color="cardColor" class="white--text" height="100%">
+    <v-row>
+      <v-col cols="5">
+        <v-img :src="isTrack ? element.album.images[0].url : ''" height="150px" contain> </v-img>
+      </v-col>
+      <v-col cols="7">
+        <v-card-title primary-title class="text-left d-block">
+          <div>
+            <div class="headline text-truncate">{{ element.name }}</div>
+            <div class="text-truncate">
+              {{ isTrack ? formattedArtistNames(element.artists) : '' }}
             </div>
-          </v-card-title>
-        </v-col>
-      </v-row>
-    </v-card>
-  </v-fade-transition>
+            <div>{{ isTrack ? element.album.release_date.split('-')[0] : '' }}</div>
+          </div>
+        </v-card-title>
+      </v-col>
+    </v-row>
+  </v-card>
 </template>
 
 <script>
