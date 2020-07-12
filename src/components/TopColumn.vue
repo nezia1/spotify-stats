49410fbe776d85bcea4433445ef64f59d51a28f4
Anthony Rodriguez <anthony@nezia.xyz>
2020-07-12 21:28:34 +0200

    Create TopColumn component

---
+++ src/components/TopColumn.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-col v-if="isMobile" class="text-center" cols="12" md="6">
+    <h1>{{ columnName }}</h1>
+    <v-col v-for="element in elements" :key="element.id">
+      <v-lazy>
+        <TopCard :element="element" :isTrack="element.type === 'track'" cardColor="cyan darken-2" />
+      </v-lazy>
+    </v-col>
+  </v-col>
+  <v-col v-else class="text-center" cols="12" md="6">
+    <h1>{{ columnName }}</h1>
+    <Scroller>
+      <v-col v-for="element in elements" :key="element.id">
+        <v-lazy>
+          <TopCard
+            :element="element"
+            :isTrack="element.type === 'track'"
+            cardColor="cyan darken-2"
+          />
+        </v-lazy>
+      </v-col>
+    </Scroller>
+  </v-col>
+</template>
+
+<script>
+import Scroller from '@/components/Scroller.vue';
+import TopCard from '@/components/TopCard.vue';
+
+export default {
+  name: 'TopColumn',
+  components: {
+    Scroller,
+    TopCard,
+  },
+  props: { columnName: String, isMobile: Boolean, elements: Array },
+  data() {
+    return {};
+  },
+};
+</script>
