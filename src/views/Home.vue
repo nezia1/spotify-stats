943d6ec174756d986725b7b6667ddf545e96f406
Anthony Rodriguez <anthony@nezia.xyz>
2020-07-13 22:32:36 +0200

    Add desktop design

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,10 +2,16 @@
   <v-container fill-height>
     <v-row align="center" justify="center">
       <v-col cols="12" class="text-center">
-        <h1>Get your favorite tracks and artists.</h1>
+        <h1 class="text-md-h2 mb-md-8">Get your favorite tracks and artists.</h1>
       </v-col>
       <v-col cols="12" class="text-center">
-        <v-btn color="#1ED760" width="90%" height="50px" class="white--text" v-on:click="login">
+        <v-btn
+          color="#1ED760"
+          :width="$vuetify.breakpoint.xs ? '90%' : '25%'"
+          height="50px"
+          class="white--text"
+          v-on:click="login"
+        >
           <v-icon left>mdi-spotify</v-icon>Login with Spotify
         </v-btn>
       </v-col>
