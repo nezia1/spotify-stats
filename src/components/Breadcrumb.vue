11f0173d3b86ba188aa794a725a40c4510b3ea6e
Anthony Rodriguez <anthony@nezia.xyz>
2020-07-21 18:13:06 +0200

    Refactor breadcrumbs into component

---
+++ src/components/Breadcrumb.vue
@@ -0,0 +1,13 @@
+<template>
+  <a :class="`text-h4 text-md-h3 ${isActive ? 'blue--text text--darken-1' : 'white--text'}`">
+    {{ title }}
+  </a>
+</template>
+
+<script>
+export default {
+  props: { title: String, isActive: Boolean },
+};
+</script>
+
+<style></style>
---
+++ src/components/Breadcrumbs.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-row align="center" justify="center">
+    <div v-for="(item, index) in items" :key="index">
+      <Breadcrumb
+        :title="item.title"
+        :isActive="item.active"
+        v-on:click.native="$emit('switch-active-item', index)"
+      />
+      <span v-if="index !== items.length - 1" class="text-h4 text-md-h3 mr-3">/</span>
+    </div>
+  </v-row>
+</template>
+
+<script>
+import Breadcrumb from '@/components/Breadcrumb.vue';
+
+export default {
+  name: 'Breadcrumbs',
+  components: {
+    Breadcrumb,
+  },
+  props: {
+    items: Array,
+  },
+};
+</script>
