9b31b4d55d1f39d0ae90de699a28d7d845f838fe
Anthony Rodriguez <anthony@nezia.xyz>
2020-07-23 16:49:53 +0200

    Add google analytics using vue-gtag

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue';
+import VueGtag from 'vue-gtag';
 import App from './App.vue';
 import router from './router';
 import vuetify from './plugins/vuetify';
 import store from './store';
+
+Vue.use(
+  VueGtag,
+  {
+    config: { id: 'UA-148445575-2' },
+  },
+  router
+);
 
 Vue.config.productionTip = false;
 
